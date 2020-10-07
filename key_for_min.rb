deeed2c0a36db08502a5817346f4d51d55a96de3
Alexander Gabriel <alexandrgabe@gmail.com>
2020-10-07 15:30:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,11 +7,13 @@ def key_for_min_value(name_hash)
   
   #binding.pry 
   
+  
+  
   name_hash.each do |key, value|
     
     binding.pry 
     
-    min_value= 
+     
     
     if name_hash[key] < min_value[key]
       
