92a0984afde1d7fe579273fef76430a144dc0c7e
Alexander Gabriel <alexandrgabe@gmail.com>
2020-10-07 15:00:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,19 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  
+  name_hash.each do |key, value|
+    
+    min_value= 
+    
+    if name_hash[key] < min_value
+      
+      min_value = name_hash[hash]
+      
+    end
+    
+    min_value
+  
+ end 
 
 end
